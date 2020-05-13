568be8b211e91b14eccd824bd41bef8c7caa9fd0
OoIwazaruoO <iwazaru@localhost>
2020-05-13 17:59:08 +0200

    callControllerMethod method added to the class Router

--- Core/Router.php
+++ Core/Router.php
@@ -15,10 +15,12 @@ class Router{
 
 		$this->url = trim($_SERVER['REQUEST_URI'], '/');
 		$this->parseUrl();
+		$this->callControllerMethod();
 
 	}
 
 	public function parseUrl(){
+
 		$this->urlData = explode('/', $this->url);
 
 		$this->setControllerFromUrlData();
@@ -28,11 +30,15 @@ class Router{
 	}
 	//set the controller if the is one in the first part of the url
 	private function setControllerFromUrlData(){
+
 		$this->controller = !empty($this->urlData[0])  ? ucfirst(strtolower($this->urlData[0])) . 'Controller' : $this->controller;
+
 	}
 	//set the controller if the is one in the second part of the url
 	private function setActionFromUrlData(){
+
 		$this->action = !empty($this->urlData[1])  ? lcfirst($this->urlData[1]) . 'Action' : $this->action;
+
 	}
 
 	//Check if there is action data in the rest of the url and set them in actionData
@@ -58,7 +64,28 @@ class Router{
 		$this->actionData->post = $_POST;
 	}
 
+	private function callControllerMethod(){
 
+		$path = '../App/Controllers/'.$this->controller.'.php';
 
+		if(file_exists($path)){
+			require_once $path;
+
+			$this->controller = new $this->controller($this->actionData);
+
+			if(method_exists($this->controller, $this->action)){
+
+				$action = $this->action;
+				$this->controller->$action();
+
+			}
+			else{
+				throw new Exception("Page introuvable");
+			}
+			
+		}else{
+			throw new Exception("Page introuvable");
+		}
+	}
 
 }
