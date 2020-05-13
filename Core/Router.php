56031b1420e1607139918a21a19b7366bbd05ec6
OoIwazaruoO <iwazaru@localhost>
2020-05-13 17:20:38 +0200

    creation of the class Router in the Core folder/namespace with his properties

---
+++ Core/Router.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core;
+
+class Router{
+
+	private $controller = "HomeController";
+	private $action = "indexAction";
+	private $url;
+	private $actionData;
+
+	public function __construct(){
+		
+	}
+
+
+
+
+
+}
