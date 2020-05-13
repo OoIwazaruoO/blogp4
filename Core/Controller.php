d16415e07f3f11073c551d2b9d5ed40d084fb541
OoIwazaruoO <iwazaru@localhost>
2020-05-13 18:36:32 +0200

    token management methods added to the controller

--- Core/Controller.php
+++ Core/Controller.php
@@ -25,5 +25,39 @@ abstract class Controller{
 		session_destroy();
 	}
 
+	public function createSessionToken()
+	{
+		$this->startSessionIfNotStarted();
+
+		if(!empty($_SESSION['tokenExpirationTime']) && !empty($_SESSION['token']))
+		{			
+			$this->certifiedToken = $this->compareTokens();
+			
+			if(time() > $_SESSION['tokenExpirationTime'])
+			{
+				unset($_SESSION['tokenExpirationTime']);
+				unset($_SESSION['token']);
+			}		
+		}
+
+		if(empty($_SESSION['tokenExpirationTime']) || empty($_SESSION['token']))
+		{
+			$_SESSION['token'] = bin2hex(random_bytes(16));
+			$_SESSION['tokenExpirationTime'] = time() + (2 * 60);
+		}
+	}
+
+	public function setToken($token = "false")
+	{
+		$this->token = !empty($token) ? $token : null;	
+	}
+
+	public function compareTokens()
+	{
+		$this->startSessionIfNotStarted();
+		
+		return empty($this->token) ? false : $this->token === $_SESSION['token'] ? true : false;
+	}
+
 
 }
