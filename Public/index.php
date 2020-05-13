1e6961aa36dd68e5cd97e30d75720f379e414883
OoIwazaruoO <iwazaru@localhost>
2020-05-13 19:46:13 +0200

    autoloaders registered in index and new router instancied

--- Public/index.php
+++ Public/index.php
@@ -1,3 +1,18 @@
 <?php
+use Core\Router;
 
-echo "Bienvenus sur le blog de jean forteroche";
+require __DIR__ . "/../Vendors/Loader/SplClassLoader.php";
+
+$coreLoader = new SplClassLoader('Core', __DIR__ . '/..');
+$controllersLoader = new SplClassLoader('Controllers', __DIR__ . '/../App');
+$managersLoader      = new SplClassLoader('Models', __DIR__ . '/../App/Models/Entities');
+$entitiesLoader     = new SplClassLoader('Entities', __DIR__ . '/../App/Models/Managers');
+
+$coreLoader->register();
+$controllersLoader->register();
+$entitiesLoader->register();
+$managersLoader->register();
+
+
+
+$app = new Router("App\\Controllers\\");
