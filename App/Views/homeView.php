0226cd6c09fc9cbcd5497d030e84ee3342849a87
OoIwazaruoO <iwazaru@localhost>
2020-05-15 13:31:56 +0200

    creation of the home page and his css

---
+++ App/Views/homeView.php
@@ -0,0 +1,19 @@
+<section id="homePresentation">
+	<h1>Voyage en Alaska</h1>
+
+	<figure class="figure">
+		<img src="/Public/images/unknow-face.png" class="figure-img img-fluid rounded" alt="Picture of Jean Forteroche ?" >
+		<figcaption class="figure-caption text-right">Jean Forteroche - l'homme aux milles blogs</figcaption>
+	</figure>
+	<p >
+		Bonjours à tous et bienvenus sur mon blog!!<br/>
+		Je me présente, Jean Forteroche pour vous écrire.<br/>
+		J'ai deux passions dans la vie: La première est d'écrire des histoires inspirantes sur l'Alaska et sa nature sauvage.<br/>
+		La seconde est de tester les compétences de développeurs web en devenir au cours de leur formation OpenClassroom. 
+	</p>
+	<blockquote class="blockquote text-center">
+		<p class="mb-0"><< S'il te plait, code moi un blog. >></p>
+		<footer class="blockquote-footer">Le petit Jean <cite title="Source Title"> Mes passions</cite></footer>
+	</blockquote>
+	<a href="/articles" ><button type="button" class="btn btn-primary">Accéder au blog n° infini²</button></a>
+</section>
