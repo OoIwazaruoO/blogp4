1d663a3208d4835325022bc626d3d5e8f859f269
OoIwazaruoO <iwazaru@localhost>
2020-05-16 15:12:27 +0200

    creation of the abstract class Entity and Manager in the Core namespace/folder

---
+++ Core/Entity.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Core;
+
+abstract class Entity{
+
+	protected $id;
+	protected $errors = [];
+
+	public function __construct(array $data = [])
+	{
+		if(!empty($data)){
+			$this->hydrate($data);
+		}	
+	}
+
+	public function hydrate($data)
+	{
+		foreach ($data as $key => $value)
+		{
+			$method = 'set'.ucfirst($key);
+
+			if (is_callable([$this, $method]))
+			{
+				$this->$method($value);
+			}
+		}
+	}
+
+	public function id(){return $this->id; }
+	public function errors(){ return $this->errors; }
+
+	public function setId($id)
+	{
+		$this->id = (int) $id;
+	}
+
+	public function isNew()
+	{
+		return empty($this->id);
+	}
+
+	public function hasErrors()
+	{
+		return !empty($this->error);
+	}
+}
---
+++ Core/Manager.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Core;
+
+abstract class Manager{
+
+	protected $dao;
+	protected $targetTable;
+	protected $className;
+
+	public function __construct($dao, $table, $classname){
+		$this->dao = $dao; 
+		$this->targetTable = $table;
+		$this->classname = $classname;
+	}
+
+	public function find($id){
+		
+		$req = $this->dao->prepare("SELECT * from {$this->targetTable} WHERE id = ?");
+
+		$req->setFetchMode(\PDO::FETCH_CLASS|\PDO::FETCH_PROPS_LATE, $this->classname);
+
+		return $req->execute(array($id));
+	}
+
+	public function findAll(){
+		$result = $this->dao->query("SELECT * from {$this->targetTable}");
+		
+		$result->setFetchMode(\PDO::FETCH_CLASS|\PDO::FETCH_PROPS_LATE, $this->classname);
+		return $result;
+	}
+
+	public function delete($id){
+		$this->dao->query("DELETE from {$this->targetTable} WHERE id = ?");	 
+	}
+}
