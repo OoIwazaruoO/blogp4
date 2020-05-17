31955d8e769fc35bee0f0547ad7ff6420ae04b1f
OoIwazaruoO <iwazaru@localhost>
2020-05-17 14:52:43 +0200

    creation of the inscriptionView and his form checker script

---
+++ App/Views/inscriptionView.php
@@ -0,0 +1,37 @@
+<section id="inscription">
+
+	<h1>Billet simple pour l'Alaska</h1>
+	<h2>Inscription</h2>
+	
+	<form action="users/register" method="post" id="inscription-form">
+		
+		<div class="form-group" >
+			<label for="pseudo">Pseudo</label>
+			<input type="text" class="form-control" id="pseudo" aria-describedby="pseudoHelp" placeholder="Saisir votre pseudo" name="pseudo" minlength="3" maxlength="12">
+			<small id="pseudoHelp" class="form-text text-muted">3 à 12 caractères; Caractères alphanumériques uniquement.</small>
+		</div>		
+		<div class="form-group">
+			<label for="mail">Adresse email</label>
+			<input type="email" class="form-control" id="mail" aria-describedby="emailHelp" placeholder="Saisir votre Email" name="mail">
+			<small id="emailHelp" class="form-text text-muted">Un email de vérification vous sera envoyé.</small>
+		</div>
+		<div class="form-group">
+			<label for="pass1">Mot de passe</label>
+			<input type="password" class="form-control" id="pass1" name="pass1" placeholder="Saisir votre mot de passe" aria-describedby="pass1Help">
+			<small id="pass1Help" class="form-text text-muted">Au moins 8 caractères</small>
+		</div>
+		<div class="form-group">
+			<label for="pass2">Mot de passe</label>
+			<input type="password" class="form-control" id="pass2" name="pass2" placeholder="Resaisir votre mot de passe" aria-describedby="pass2Help">
+			<small id="pass2Help" class="form-text text-muted">Resaisir le mot de passe.</small>
+		</div>
+		
+		<div class="alert alert-info" role="alert" id="form-info">
+
+		</div>
+		<button type="submit" class="btn btn-primary" id="submitInscription">Submit</button>
+
+	</form>
+
+</section>
+
---
+++ Public/js/checkInscription.js
@@ -0,0 +1,30 @@
+$('#submitInscription').click((e) => {
+	e.preventDefault();
+
+	let checkMessage = null;
+
+	let mailReg = /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/;
+
+	let pseudo = $('#pseudo').val();
+	let mail = $('#mail').val();
+	let pass1 = $('#pass1').val();
+	let pass2 = $('#pass2').val();
+
+	if (pseudo.length < 3) {
+		checkMessage = "Pseudo trop court : au moins 3 caractères";
+	} else if (pseudo.length > 12) {
+		checkMessage = 'Pseudo trop long : 12 caractères max';
+	} else if (!mailReg.test(String(mail).toLowerCase())) {
+		checkMessage = 'Le mail est invalide';
+	} else if (pass1.length < 8) {
+		checkMessage = 'Mot de passe trop court';
+	} else if (pass1 !== pass2) {
+		checkMessage = 'Les mots de passe doivent être identiques';
+	} else {
+		 $('#inscription-form').submit();
+	}
+
+	if (checkMessage) {
+		$('#form-info').text(checkMessage);
+	}
+});
