9abaaea21a6535698efe9ef107b455e75700bcf8
OoIwazaruoO <iwazaru@localhost>
2020-05-20 19:26:36 +0200

    Creation of legalController and legalView

---
+++ App/Controllers/LegalController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controllers;
+
+use Core\MyController;
+
+class LegalController extends MyController {
+
+	public function __construct($actionData) {
+
+		parent::__construct($actionData);
+
+	}
+
+	public function indexAction() {
+
+		$website = "addresse web";
+		$owner = "Nom du propriétaire";
+		$ownerAddress = "addresse du propriétaire";
+		$ownerMail = "mail du propriétaire";
+
+		$this->view->render("legal", compact('website', 'owner', 'ownerAddress', 'ownerMail'));
+
+	}
+}
