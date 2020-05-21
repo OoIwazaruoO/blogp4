56cbbeb4f5bf145f2d49fa068990182ec0f59758
OoIwazaruoO <iwazaru@localhost>
2020-05-21 21:51:39 +0200

    added missing file for previous commit

--- App/Models/Managers/ArticlesManager.php
+++ App/Models/Managers/ArticlesManager.php
@@ -2,14 +2,27 @@
 
 namespace Managers;
 
-use Entities\Article;
 use Core\Manager;
 
-class ArticlesManager extends Manager{
+class ArticlesManager extends Manager {
 
-	public function __construct($dao, $table, $classname){
+	public function __construct($dao, $table, $classname) {
 		parent::__construct($dao, $table, $classname);
 	}
 
+	public function add($title, $chapterNumber, $content, $type, $pictureName = null) {
+
+		if ($pictureName != null):
+			$sql = "INSERT INTO {$this->targetTable}(title, content, type, chapterNumber, pictureName) VALUES(:title, :content, :type, :chapterNumber, :pictureName)";
+			$execData = array(":title" => $title, ":content" => $content, ":type" => $type, ":chapterNumber" => $chapterNumber, ":pictureName" => $pictureName);
+		else:
+			$sql = "INSERT INTO {$this->targetTable}(title, content, type, chapterNumber) VALUES(:title, :content, :type, :chapterNumber)";
+			$execData = array(":title" => $title, ":content" => $content, ":type" => $type, ":chapterNumber" => $chapterNumber);
+		endif;
+
+		$req = $this->dao->prepare($sql);
+		$req->execute($execData);
+
+	}
 
 }
